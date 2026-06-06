fccf7643e0eb0f88b91767f5298349410aca8a65
Lauris Taube <lauristaube@gmail.com>
2026-06-06 09:40:37 +0300

    car controller to quest input try

--- Assets/_GAME/Scripts/Car/Controller.cs
+++ Assets/_GAME/Scripts/Car/Controller.cs
@@ -6,14 +6,24 @@ namespace Assets._GAME.Scripts.Car
 {
     public class Controller : MonoBehaviour
     {
+        [SerializeField] private G27QuestController wheelController;
+
         public Suspension suspension;
         public Engine engine;
         public Steering steering;
 
         private void FixedUpdate()
         {
+            if (wheelController == null || !wheelController.IsConnected) return;
+            
             float vertical = Input.GetAxis("Vertical");
             float horizontal = Input.GetAxis("Horizontal");
+            
+            float steerInput = wheelController.Steering;
+            float gasInput = wheelController.Throttle;
+            float brakeInput = wheelController.Brake;
+            
+            
             suspension.ApplySpringForce(vertical);
             if (!suspension.grounded) return;
             engine.Run(vertical);
